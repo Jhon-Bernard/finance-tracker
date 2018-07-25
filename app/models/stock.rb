e76c843776b23de102d0fd61f3bba0c04effa11e
John <bernard.baclay@go-va.com.au>
2018-07-25 17:46:11 +0800

    New changes

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord
 
   def self.new_from_lookup(ticker_symbol)
     thestock = StockQuote::Stock.quote(ticker_symbol)
-    Stock.new(name: thestock.company_name, ticker: thestock.symbol, last_price: thestock.latest_price)
+    new(name: thestock.name, ticker: thestock.symbol, last_price: thestock.latest_price)
   end
 
 end
